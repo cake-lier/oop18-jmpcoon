a21e61bb5a5306fba561f534d34aa75dc8561cf3
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-03-05 22:04:43 +0100

    Add DynamicPhysicalBody.java

---
+++ src/model/DynamicPhysicalBody.java
@@ -0,0 +1,29 @@
+package model;
+
+import org.jbox2d.dynamics.Body;
+
+import model.AbstractPhysicalBody;
+import model.State;
+/**
+ * a class representing a {@link PhysicalBody} that can move (players and enemies).
+ */
+public class DynamicPhysicalBody extends AbstractPhysicalBody {
+
+    /**
+     * builds a new {@link DynamicPhysicalBody}.
+     * @param body the {@link Body} encapsulated by this {@link DynamicPhysicalBody}
+     */
+    public DynamicPhysicalBody(final Body body) {
+        super(body);
+    }
+
+    //TODO: state defined from the class movement (?)
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public State getState() {
+        return null;
+    }
+
+}
