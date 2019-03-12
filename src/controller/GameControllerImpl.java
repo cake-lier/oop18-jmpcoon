03d6483a845b0537c71c68862adbbdd37f73c659
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2019-03-12 19:25:58 +0100

    Implement start() and pause() methods in GameControllerImpl.java

--- src/controller/GameControllerImpl.java
+++ src/controller/GameControllerImpl.java
@@ -1,37 +1,53 @@
 package controller;
 
 import java.util.Collection;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import model.Entity;
 import model.MovementType;
 import model.World;
 import model.WorldImpl;
 import utils.Pair;
+import view.game.GameView;
 
 /**
  * a {@link GameController} for a game set in a {@link World}.
  */
 public class GameControllerImpl implements GameController {
 
+    private static final long DELTA_UPDATE = 15;
+
     private final World gameWorld;
+    private final GameView gameView;
+    private final ScheduledThreadPoolExecutor timer;
 
     /**
      * builds a new {@link GameControllerImpl}.
+     * @param view the {@link GameView} relative to the game controlled by this {@link GameController}
      */
-    public GameControllerImpl() {
+    public GameControllerImpl(final GameView view) {
         this.gameWorld = new WorldImpl();
+        this.gameView = view;
+        // Runtime.getRuntime().availableProcessors() + 1 is the size of the pool of threads 
+        this.timer = new ScheduledThreadPoolExecutor(Runtime.getRuntime().availableProcessors() + 1);
+        this.timer.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void startGame() {
-        // TODO Auto-generated method stub
-
+        this.timer.scheduleWithFixedDelay(() -> updateWorldAndView(), DELTA_UPDATE, DELTA_UPDATE, TimeUnit.MILLISECONDS);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void pauseGame() {
-        // TODO Auto-generated method stub
-
+        this.timer.shutdown();
     }
 
     @Override
@@ -83,6 +99,11 @@ public class GameControllerImpl implements GameController {
     @Override
     public Pair<Double, Double> getWorldDimensions() {
         return this.gameWorld.getDimensions();
+    }
+
+    private void updateWorldAndView() {
+        this.gameWorld.update();
+        this.gameView.update();
     }
 
 }
---
+++ src/view/game/GameView.java
@@ -0,0 +1,8 @@
+package view.game;
+
+
+public interface GameView {
+
+    void update();    
+
+}
