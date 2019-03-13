8a8d55ffc4e825542f5dc0ebb42d3fda1fd73cf7
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-03-13 19:42:24 +0100

    Add createDynamicPhysicalBody method to PhysicalFactory.java

--- src/model/PhysicalFactory.java
+++ src/model/PhysicalFactory.java
@@ -1,11 +1,10 @@
 package model;
 
-import com.sun.webkit.ThemeClient;
-
 import utils.Pair;
 
 /**
- * a interface for a class used to produce {@link PhysicalWorld} and {@link PhysicalBody}.
+ * a interface for a class used to produce {@link PhysicalWorld} and
+ * {@link PhysicalBody}.
  */
 public interface PhysicalFactory {
 
@@ -16,16 +15,40 @@ public interface PhysicalFactory {
 
     /**
      * creates {@link StaticPhysicalBody}.
-     * @param position the upper left corner of the {@link StaticPhysicalBody} created
-     * @param angle the angle in radians of the created {@link StaticPhysicalBody}
-     * @param shape the {@link EntityShape} of {@link StaticPhysicalBody} created
-     * @param width 
+     * 
+     * @param position
+     *            the upper left corner of the {@link StaticPhysicalBody} created
+     * @param angle
+     *            the angle in radians of the created {@link StaticPhysicalBody}
+     * @param shape
+     *            the {@link EntityShape} of {@link StaticPhysicalBody} created
+     * @param width
      * @param height
-     * @param type the {@link EntityType} of the {@link Entity} that will use the created {@link StaticPhysicalBody}
+     * @param type
+     *            the {@link EntityType} of the {@link Entity} that will use the created {@link StaticPhysicalBody}
      * @return a {@link StaticPhysicalBody} with the given characteristics
      */
-    StaticPhysicalBody createStaticPhysicalBody(Pair<Double, Double> position, double angle, EntityShape shape, double width, double height, EntityType type);
+    StaticPhysicalBody createStaticPhysicalBody(Pair<Double, Double> position, double angle, EntityShape shape,
+            double width, double height, EntityType type);
 
-    // TODO: add method to create DynamicPhysicalBody
+    /**
+     * creates {@link DynamicPhysicalBody}.
+     * 
+     * @param position
+     *            the upper left corner of the {@link DynamicPhysicalBody} created
+     * @param angle
+     *            the angle in radians of the created {@link DynamicPhysicalBody}
+     * @param shape
+     *            the {@link EntityShape} of {@link DynamicPhysicalBody} created
+     * @param width
+     *            the width of the Covex Shape of the {@link DynamicPhysicalBody}
+     * @param height
+     *            the height of the Covex Shape of the {@link DynamicPhysicalBody}
+     * @param type
+     *            the {@link EntityType} of the {@link Entity} that will use the created {@link DynamicPhysicalBody}
+     * @return a {@link DynamicPhysicalBody} with the given characteristics
+     */
+    DynamicPhysicalBody createDynamicPhysicaBody(Pair<Double, Double> position, double angle, EntityShape shape,
+            double width, double height, EntityType type);
 
 }
