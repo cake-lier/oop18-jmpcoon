5a2b237d4ea87fc3094b4718873c76d130270c16
Elena Rughi <elena.rughi2@studio.unibo.it>
2019-03-18 14:17:26 +0100

    Implement update() method in GameViewImpl.java

--- src/view/game/GameView.java
+++ src/view/game/GameView.java
@@ -1,10 +1,23 @@
 package view.game;
 
-
+/**
+ * Visual component of the game.
+ */
 public interface GameView {
 
+    /**
+     * Uses all current entities stored in {@link GameController} to update the game scene.
+     */
     void update();
+
+    /**
+     * Shows game over screen.
+     */
     void showGameOver();
+
+    /**
+     * Shows win screen.
+     */
     void showPlayerWin();
-    
+
 }
--- src/view/game/GameViewImpl.java
+++ src/view/game/GameViewImpl.java
@@ -8,60 +8,82 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.Collection;
+
 import controller.InputType;
 import controller.app.AppController;
 import controller.game.GameController;
 import controller.game.GameControllerImpl;
 
+/**
+ * The class implementation of the {@link GameView} interface.
+ */
 public class GameViewImpl implements GameView {
+    private static final int FONTSIZE = 150;
+
     private final GameController gameController;
     private final AppController appController;
     private final Stage stage;
     private final Scene scene;
-    private final Group root;
-    
+
+    /**
+     * Binds this game view to the instance of the {@link AppController},
+     * acquires the {@link Stage} in which to draw the game,
+     * creates an instance of the {@link GameController}.
+     * @param appController The application controller.
+     * @param stage The stage in which to draw the game scene.
+     */
     public GameViewImpl(final AppController appController, final Stage stage) {
-        this.appController=appController;
-        this.gameController=new GameControllerImpl(this.appController, this);
-        this.stage=stage;
-        root = new Group();
-        this.scene=new Scene(root, stage.getHeight(), stage.getWidth(), Color.BLACK);
+        this.appController = appController;
+        this.gameController = new GameControllerImpl(this.appController, this);
+        this.stage = stage;
+        this.scene = new Scene(new Group(), stage.getHeight(), stage.getWidth(), Color.GREEN);
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> getInput(key.getCode()));
+        stage.setScene(scene);
         }
-    
 
-    @Override
+    /**
+     *{@inheritDoc}
+     */
     public void update() {
-        // TODO Auto-generated method stub    
-    }
-    
-    private void getInput(KeyCode key) {
-        InputType it;
-        switch(key) {
-            case W: it=InputType.CLIMB; break;
-            case A: it=InputType.LEFT; break;
-            case D: it=InputType.RIGHT; break;
-            case SPACE: it=InputType.UP; break;
-            default: it=InputType.RIGHT; break;
-        }
-        gameController.processInput(it);
+        final Collection<DrawableEntity> drawableEntities = gameController.getDrawableEntities();
+        final Group root = new Group();
+        drawableEntities.forEach(e -> {
+            root.getChildren().add(e.getImageView());
+        });
+        scene.setRoot(root);
     }
 
-
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public void showGameOver() {
-        final Text text = new Text(450, stage.getHeight()/2, "Game Over");
-        text.setFont(new Font(150));
+        final Text text = new Text(450, stage.getHeight() / 2, "Game Over");
+        text.setFont(new Font(FONTSIZE));
         final Scene gameOver = new Scene(new Group(text), stage.getHeight(), stage.getWidth(), Color.DARKRED);
         stage.setScene(gameOver);
     }
 
-
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public void showPlayerWin() {
-        final Text text = new Text(450, stage.getHeight()/2, "You won!"); 
-        text.setFont(new Font(150));
+        final Text text = new Text(450, stage.getHeight() / 2, "You won!"); 
+        text.setFont(new Font(FONTSIZE));
         final Scene win = new Scene(new Group(text), stage.getHeight(), stage.getWidth(), Color.DARKRED);
         stage.setScene(win);
+    }
+
+    private void getInput(final KeyCode key) {
+        InputType it;
+        switch (key) {
+            case W: it = InputType.CLIMB; break;
+            case A: it = InputType.LEFT; break;
+            case D: it = InputType.RIGHT; break;
+            case SPACE: it = InputType.UP; break;
+            default: it = InputType.RIGHT; break;
+        }
+        gameController.processInput(it);
     }
 }
