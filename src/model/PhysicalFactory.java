ea3bdbf8b91ccf6e4420bc93f68a66721b6313b1
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-03-18 19:08:25 +0100

    Edit javadoc comments of PhysicalFactory.java

--- src/model/PhysicalFactory.java
+++ src/model/PhysicalFactory.java
@@ -4,11 +4,16 @@ import utils.Pair;
 
 /**
  * a interface for a class used to produce {@link PhysicalWorld} and
- * {@link PhysicalBody}. The {@link PhysicalBody} must be created after the {@link PhysicalWorld}.
+ * {@link PhysicalBody}. The {@link PhysicalBody} must be created after the
+ * {@link PhysicalWorld}.
  */
 public interface PhysicalFactory {
 
     /**
+     * @param width
+     *            the width of the physical world
+     * @param height
+     *            the height of the physical world
      * @return the {@link PhysicalWorld} in which all the {@link PhysicalBody} lives
      */
     PhysicalWorld createPhysicalWorld(double width, double height);
@@ -22,15 +27,18 @@ public interface PhysicalFactory {
      *            the angle in radians of the created {@link StaticPhysicalBody}
      * @param shape
      *            the {@link EntityShape} of {@link StaticPhysicalBody} created
-     * @param width the width of the physical body
-     * @param height the height of the physical body
-     * @param type the {@link EntityType} of the {@link Entity} that will use the created {@link StaticPhysicalBody}
+     * @param width
+     *            the width of the physical body
+     * @param height
+     *            the height of the physical body
+     * @param type
+     *            the {@link EntityType} of the {@link Entity} that will use the
+     *            created {@link StaticPhysicalBody}
      * @return a {@link StaticPhysicalBody} with the given characteristics
      */
     StaticPhysicalBody createStaticPhysicalBody(Pair<Double, Double> position, double angle, EntityShape shape,
             double width, double height, EntityType type);
 
-    // TODO: Convex Shape is related to the library used, it shouldn't appear in the comments of the interface
     /**
      * creates {@link DynamicPhysicalBody}.
      * 
@@ -41,11 +49,12 @@ public interface PhysicalFactory {
      * @param shape
      *            the {@link EntityShape} of {@link DynamicPhysicalBody} created
      * @param width
-     *            the width of the Convex Shape of the {@link DynamicPhysicalBody}
+     *            the width of the physical body
      * @param height
-     *            the height of the Convex Shape of the {@link DynamicPhysicalBody}
+     *            the height of the physical body
      * @param type
-     *            the {@link EntityType} of the {@link Entity} that will use the created {@link DynamicPhysicalBody}
+     *            the {@link EntityType} of the {@link Entity} that will use the
+     *            created {@link DynamicPhysicalBody}
      * @return a {@link DynamicPhysicalBody} with the given characteristics
      */
     DynamicPhysicalBody createDynamicPhysicaBody(Pair<Double, Double> position, double angle, EntityShape shape,
