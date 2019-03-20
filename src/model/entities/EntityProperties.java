277a90c3f8173e6204a67367a068f9e59748af31
cake_lier <matteo.castellucci14@hotmail.it>
2019-03-20 11:11:08 +0100

    Add dimensions as properties in an EntityProperties

--- src/model/entities/EntityProperties.java
+++ src/model/entities/EntityProperties.java
@@ -5,26 +5,35 @@ import java.io.Serializable;
 import org.apache.commons.lang3.tuple.Pair;
 
 /**
- * Contains the properties of an {@link Entity} which has to be created, such as the
- * {@link EntityType}, the {@link EntityShape}, the current position in the {@link World}
- * and its angle with the x axis.
+ * Contains the properties of an {@link Entity} which has to be created, such as the {@link EntityType}, the {@link EntityShape},
+ * the current position in the {@link World} and its angle with the x axis.
  */
 public interface EntityProperties extends Serializable {
+
     /**
      * Getter for the {@link EntityType} property.
      * @return The {@link EntityType} property value.
      */
     EntityType getEntityType();
+
     /**
      * Getter for the {@link EntityShape} property.
      * @return The {@link EntityShape} property value.
      */
     EntityShape getEntityShape();
+
     /**
-     * Getter for the position of this {@link Entity} in {@link World} coordinates.
+     * Getter for the position of the upper left corner of this {@link Entity} in world coordinates.
      * @return A {@link Pair} containing the values of x and y coordinates.
      */
     Pair<Double, Double> getPosition();
+
+    /**
+     * Getter for the width and the height of this {@link Entity}.
+     * @return A {@link Pair} containing the width and the height.
+     */
+    Pair<Double, Double> getDimensions();
+
     /**
      * Getter for the angle property.
      * @return The angle value in radians.
--- src/model/entities/EntityPropertiesImpl.java
+++ src/model/entities/EntityPropertiesImpl.java
@@ -11,28 +11,33 @@ import com.google.common.hash.Hashing;
  * Class implementation of {@link EntityProperties}.
  */
 public final class EntityPropertiesImpl implements EntityProperties {
-    private static final long serialVersionUID = 6911353545986657204L;
+    private static final long serialVersionUID = -6863412268735663647L;
 
     private final EntityType type;
     private final EntityShape shape;
     private final Pair<Double, Double> position;
+    private final Pair<Double, Double> size;
     private final double angle;
+
     /**
      * Collects the properties of the associated {@link Entity}.
-     * @param type The {@link EntityType} of the associated entity.
-     * @param shape The {@link EntityShape} of the associated entity.
-     * @param xCoord The x coordinate of the associated entity.
-     * @param yCoord The y coordinate of the associated entity.
+     * @param type The {@link EntityType} of the associated {@link Entity}.
+     * @param shape The {@link EntityShape} of the associated {@link Entity}.
+     * @param xCoord The x coordinate of the associated {@link Entity}.
+     * @param yCoord The y coordinate of the associated {@link Entity}.
+     * @param width The width of the associated {@link Entity}.
+     * @param height The height of the associated {@link Entity}.
      * @param angle The angle of the associated entity.
      */
-    public EntityPropertiesImpl(final EntityType type, final EntityShape shape,
-                                final double xCoord, final double yCoord,
-                                final double angle) {
+    public EntityPropertiesImpl(final EntityType type, final EntityShape shape, final double xCoord, final double yCoord,
+                                final double width, final double height, final double angle) {
         this.type = type;
         this.shape = shape;
         this.position = new ImmutablePair<>(xCoord, yCoord);
+        this.size = new ImmutablePair<Double, Double>(width, height);
         this.angle = angle;
     }
+
     /**
      * {@inheritDoc}
      */
@@ -40,6 +45,7 @@ public final class EntityPropertiesImpl implements EntityProperties {
     public EntityType getEntityType() {
         return this.type;
     }
+
     /**
      * {@inheritDoc}
      */
@@ -47,6 +53,7 @@ public final class EntityPropertiesImpl implements EntityProperties {
     public EntityShape getEntityShape() {
         return this.shape;
     }
+
     /**
      * {@inheritDoc}
      */
@@ -54,6 +61,15 @@ public final class EntityPropertiesImpl implements EntityProperties {
     public Pair<Double, Double> getPosition() {
         return this.position;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Pair<Double, Double> getDimensions() {
+        return this.size;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -61,6 +77,7 @@ public final class EntityPropertiesImpl implements EntityProperties {
     public double getAngle() {
         return this.angle;
     }
+
     /**
      * {@inheritDoc}
      */
@@ -72,9 +89,11 @@ public final class EntityPropertiesImpl implements EntityProperties {
                       .putInt(this.position.hashCode())
                       .putInt(this.shape.hashCode())
                       .putInt(this.type.hashCode())
+                      .putInt(this.size.hashCode())
                       .hash()
                       .asInt();
     }
+
     /**
      * {@inheritDoc}
      */
@@ -92,14 +111,16 @@ public final class EntityPropertiesImpl implements EntityProperties {
         final EntityPropertiesImpl other = (EntityPropertiesImpl) obj;
         return Double.doubleToLongBits(this.angle) == Double.doubleToLongBits(other.angle)
                 && Objects.equals(this.position, other.position)
+                && Objects.equals(this.size, other.size)
                 && this.shape == other.shape && this.type == other.type;
     }
+
     /** 
      * {@inheritDoc}
      */
     @Override
     public String toString() {
         return "EntityPropertiesImpl [type=" + this.type + ", shape=" + this.shape
-               + ", position=" + this.position + ", angle=" + this.angle + "]";
+               + ", position=" + this.position + ", dimensions=" + this.size + ", angle=" + this.angle + "]";
     }
 }
