880ab0661c568249bd793b65dcc9047ac5957288
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-03-24 16:27:08 +0100

    Implement updateSpriteProperties() in DynamicDrawableEntity.java

--- src/view/game/DynamicDrawableEntity.java
+++ src/view/game/DynamicDrawableEntity.java
@@ -2,14 +2,14 @@ package view.game;
 
 import javafx.scene.image.Image;
 import model.DynamicEntity;
+import model.Entity;
 import utils.Pair;
+import utils.PairImpl;
 
 /**
  * a {@link DynamicEntity} that can be drawn.
  */
 public class DynamicDrawableEntity extends AbstractDrawableEntity {
-
-    private final SpriteAnimation animation;
 
     /**
      * builds a new {@link StaticDrawableEntity}.
@@ -22,13 +22,10 @@ public class DynamicDrawableEntity extends AbstractDrawableEntity {
      *            the dimensions of the {@link World} in which the {@link Entity} lives
      * @param sceneDimensions
      *            the dimensions of the view in which this {@link DynamicDrawableEntity} will be drawn
-     * @param SpriteAnimation
-     *            the animation associated with this {@link DynamicDrawableEntity}
      */
     public DynamicDrawableEntity(final String spriteUrl, final DynamicEntity entity,
-            final Pair<Double, Double> worldDimensions, final Pair<Double, Double> sceneDimensions, final SpriteAnimation animation) {
+            final Pair<Double, Double> worldDimensions, final Pair<Double, Double> sceneDimensions) {
         super(spriteUrl, entity, worldDimensions, sceneDimensions);
-        this.animation = animation;
     }
 
     /**
@@ -42,18 +39,32 @@ public class DynamicDrawableEntity extends AbstractDrawableEntity {
      *            the dimensions of the {@link World} in which the {@link Entity} lives
      * @param sceneDimensions
      *            the dimensions of the view in which this {@link DynamicDrawableEntity} will be drawn
-     * @param SpriteAnimation
-     *            the animation associated with this {@link DynamicDrawableEntity}
      */
     public DynamicDrawableEntity(final Image image, final DynamicEntity entity,
-            final Pair<Double, Double> worldDimensions, final Pair<Double, Double> sceneDimensions, final SpriteAnimation animation) {
+            final Pair<Double, Double> worldDimensions, final Pair<Double, Double> sceneDimensions) {
         super(image, entity, worldDimensions, sceneDimensions);
-        this.animation = animation;
     }
-
+ 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void updateSpriteProperties() {
-        
-    }
+        final Entity entity = this.getEntity();
+        final double x = entity.getPosition().getX();
+        final double y = entity.getPosition().getY();
+        final double width = entity.getDimensions().getX();
+        final double height = entity.getDimensions().getY();
 
+        this.getImageView().setScaleX(width * this.getXRatio() / this.getImageView().getImage().getWidth());
+        this.getImageView().setScaleY(height * this.getYRatio() / this.getImageView().getImage().getHeight());
+        this.getImageView().setRotate(-Math.toDegrees(this.getEntity().getAngle()));
+
+        /* differences between the sizes of the ImageView and of the image really shown */
+        final double diffX = this.getImageView().getImage().getWidth() - width * this.getXRatio();
+        final double diffY = this.getImageView().getImage().getHeight() - height * this.getYRatio();
+        final Pair<Double, Double> sceneCoordinates = this.getConvertedCoordinates(new PairImpl<>(x - width / 2, y + height / 2));
+        this.getImageView().setX(sceneCoordinates.getX() - diffX / 2);
+        this.getImageView().setY(sceneCoordinates.getY() - diffY / 2);
+    }
 }
