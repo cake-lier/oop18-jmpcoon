55a7bdf9cad35bcf4db5a73bc87ecc2d08226f0e
cake_lier <matteo.castellucci14@hotmail.it>
2019-03-27 12:35:05 +0100

    Add background to game scene

--- src/view/game/GameViewImpl.java
+++ src/view/game/GameViewImpl.java
@@ -2,8 +2,16 @@ package view.game;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -21,6 +29,7 @@ import controller.game.GameControllerImpl;
  */
 public class GameViewImpl implements GameView {
     private static final int FONTSIZE = 150;
+    private static final String BG_SOURCE = "images/bg_game.png";
 
     private final GameController gameController;
     private final AppController appController;
@@ -38,9 +47,22 @@ public class GameViewImpl implements GameView {
         this.appController = appController;
         this.gameController = new GameControllerImpl(this, this.appController);
         this.stage = stage;
-        this.scene = new Scene(new Group(), stage.getHeight(), stage.getWidth(), Color.WHITE);
+        final Pane root = new Pane();
+        this.addBackgroundImage(root);
+        this.scene = new Scene(root, this.stage.getScene().getWidth(), this.stage.getScene().getHeight());
         this.scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> getInput(key.getCode()));
         this.stage.setScene(this.scene);
+    }
+
+    /*
+     * Sets the background of the passed Pane with a chosen image. It's positioned at the center of the scene,
+     * stretched to cover the whole scene.
+     */
+    private void addBackgroundImage(final Pane root) {
+        final Image bgImage = new Image(BG_SOURCE);
+        final BackgroundSize bgSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, false, true); 
+        final BackgroundImage bgImagePositioned = new BackgroundImage(bgImage, BackgroundRepeat.ROUND, BackgroundRepeat.ROUND, BackgroundPosition.CENTER, bgSize);
+        root.setBackground(new Background(bgImagePositioned));
     }
 
     /**
@@ -48,8 +70,9 @@ public class GameViewImpl implements GameView {
      */
     public void update() {
         final EntityConverter entityConverter = new EntityConverter(this.gameController.getWorldDimensions(),
-                                                                    new ImmutablePair<>(this.stage.getWidth(), this.stage.getHeight()));
-        final Group root = new Group();
+                                                                    new ImmutablePair<>(this.scene.getWidth(), this.scene.getHeight()));
+        final Pane root = new Pane();
+        this.addBackgroundImage(root);
         this.gameController.getEntities()
                            .stream()
                            .map(entity -> entityConverter.getDrawableEntity(entity))
