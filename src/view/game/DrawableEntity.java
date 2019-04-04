fac0e68a8e02fb410aa1819d1bc6a7a00ff714b5
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-04-04 22:15:37 +0200

    Remove updatePosition() in DrawableEntity.java

--- src/view/game/DrawableEntity.java
+++ src/view/game/DrawableEntity.java
@@ -8,12 +8,6 @@ import javafx.scene.image.ImageView;
 public interface DrawableEntity {
 
     /**
-     * updates the position of the {@link ImageView} of this {@link DrawableEntity}
-     * so that it's up to date with the one of the {@link Entity}.
-     */
-    void updatePosition();
-
-    /**
      * @return the {@link ImageView} representing the {@link Entity}
      */
     ImageView getImageView();
