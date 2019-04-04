62dab8ca278a07aa90c6b0b0af0278edf626d381
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-04-04 22:47:01 +0200

    Implement updateSpriteProperties() in AbstractDrawableEntity.java

--- src/view/game/AbstractDrawableEntity.java
+++ src/view/game/AbstractDrawableEntity.java
@@ -46,9 +46,24 @@ public abstract class AbstractDrawableEntity implements DrawableEntity {
     }
 
     /**
-     * method used to update properties like position, rotation, ... of the {@link ImageView} of this {@link DrawableEntity}
+     * updates properties like position, rotation, ... of the {@link ImageView} of this {@link DrawableEntity}
      */
-    protected abstract void updateSpriteProperties();
+    protected void updateSpriteProperties() {
+        final double entityWidth = this.getEntity().getDimensions().getLeft();
+        final double entityHeight = this.getEntity().getDimensions().getRight();
+        final double entityX = this.getEntity().getPosition().getLeft();
+        final double entityY = this.getEntity().getPosition().getRight();
+        /* scaling the ImageView to correct dimensions */
+        this.getImageView().setScaleX(entityWidth * this.getXRatio() / this.getImageView().getImage().getWidth());
+        this.getImageView().setScaleY(entityHeight * this.getYRatio() / this.getImageView().getImage().getHeight());
+        this.getImageView().setRotate(-Math.toDegrees(this.getEntity().getAngle()));
+        /* differences between the sizes of the ImageView and of the image really shown */
+        final double diffX = this.getImageView().getImage().getWidth() - entityWidth * this.getXRatio();
+        final double diffY = this.getImageView().getImage().getHeight() - entityHeight * this.getYRatio();
+        final Pair<Double, Double> sceneCoordinates = this.getConvertedCoordinates(new ImmutablePair<>(entityX - entityWidth / 2, entityY + entityHeight / 2));
+        this.getImageView().setX(sceneCoordinates.getLeft() - diffX / 2);
+        this.getImageView().setY(sceneCoordinates.getRight() - diffY / 2);
+    }
 
     /**
      * @return the {@link Entity} represented by this {@link DrawableEntity}
