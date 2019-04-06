84e37efff729ee214159b5f6f00a0759d3264d15
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-04-06 19:57:42 +0200

    Edit PlayerView constructor

--- src/view/game/PlayerView.java
+++ src/view/game/PlayerView.java
@@ -3,7 +3,7 @@ package view.game;
 import org.apache.commons.lang3.tuple.Pair;
 import javafx.scene.image.Image;
 import javafx.util.Duration;
-import model.entities.DynamicEntity;
+import model.entities.Player;
 import model.entities.State;
 
 /**
@@ -20,16 +20,16 @@ public class PlayerView extends DynamicDrawableEntity {
     private static final int DURATION = 700;
 
     /**
-     * @param entity
+     * @param player
      *            the {@link Player}
      * @param worldDimensions
      *            the dimensions of the {@link World}
      * @param sceneDimensions
      *            the dimensions of the view in which this {@link Player} will be drawn
      */
-    public PlayerView(final DynamicEntity entity,
+    public PlayerView(final Player player,
             final Pair<Double, Double> worldDimensions, final Pair<Double, Double> sceneDimensions) {
-        super(IDLE, entity, worldDimensions, sceneDimensions);
+        super(IDLE, player, worldDimensions, sceneDimensions);
         this.mapAnimation(State.IDLE, new SpriteAnimation(IDLE, Duration.millis(DURATION), IDLE_FRAMES, WIDTH, HEIGHT));
         this.mapAnimation(State.MOVING_RIGHT, new SpriteAnimation(WALKING, Duration.millis(DURATION), WALKING_FRAMES, WIDTH, HEIGHT));
         this.mapAnimation(State.MOVING_LEFT, new SpriteAnimation(WALKING, Duration.millis(DURATION), WALKING_FRAMES, WIDTH, HEIGHT));
