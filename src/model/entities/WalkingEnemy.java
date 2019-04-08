821f012f3c501591e74509b0ea2f3b4927ec57d3
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-04-08 21:33:37 +0200

    Rework computeMovement() to be independent from the position in WalkingEnemy.java

--- src/model/entities/WalkingEnemy.java
+++ src/model/entities/WalkingEnemy.java
@@ -6,21 +6,23 @@ import model.physics.DynamicPhysicalBody;
  * a walking enemy inside the {@link World} of the game.
  */
 public final class WalkingEnemy extends DynamicEntity {
-    private static final long serialVersionUID = 5020187009003425168L;
-    private static final double DISTANCE = 5;
-    private static final double WALKING_SPEED = 4;
 
-    private final double initialPosition;
+    private static final long serialVersionUID = 5020187009003425168L;
+    private static final double WALKING_SPEED = 1;
+    private static final double DELTA = 0.055;
+
+    private double count = 0;
     private final DynamicPhysicalBody body;
 
     /**
      * builds a new {@link WalkingEnemy}.
-     * @param body the {@link PhysicalBody} of this {@link WalkingEnemy}
+     * 
+     * @param body
+     *            the {@link PhysicalBody} of this {@link WalkingEnemy}
      */
     public WalkingEnemy(final DynamicPhysicalBody body) {
         super(body);
         this.body = body;
-        this.initialPosition = this.body.getPosition().getLeft();
     }
 
     /**
@@ -43,13 +45,25 @@ public final class WalkingEnemy extends DynamicEntity {
      * computes the backward-and-forward movement.
      */
     public void computeMovement() {
-        final double x = this.body.getPosition().getLeft();
-        if (x <= this.initialPosition) {
+        applyImpulse(direction());
+    }
+
+    private void applyImpulse(final boolean direction) {
+        if (direction) {
             body.applyMovement(MovementType.MOVE_RIGHT, WALKING_SPEED, 0);
-        }
-        if (x >= this.initialPosition + DISTANCE) {
+        } else {
             body.applyMovement(MovementType.MOVE_LEFT, -WALKING_SPEED, 0);
         }
     }
 
+    private boolean direction() {
+        return Math.sin(counter()) >= 0;
+    }
+
+    private double counter() {
+        this.count = count + DELTA;
+        return this.count;
+    }
+
 }
+
