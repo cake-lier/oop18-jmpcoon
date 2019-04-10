4fec1899ae596790fe40b293f8620165c4cd4795
cake_lier <matteo.castellucci14@hotmail.it>
2019-04-10 10:19:16 +0200

    Refine how dimensions of sprites are calculated

--- src/view/game/PlayerView.java
+++ src/view/game/PlayerView.java
@@ -13,11 +13,12 @@ import model.entities.State;
  */
 public class PlayerView extends DynamicDrawableEntity {
 
-    private static final int DURATION = 700;
+    private static final int DURATION = 1400;
 
     /**
      * @param spritesheets A map that matches the {@link State} of the drawn {@link Entity} to the spritesheets that will 
-     * represent it. The value of an entry is a pair with the spritesheet and the number of frames it contains. 
+     * represent it. The value of an entry is a pair with the spritesheet and the number of frames it contains. There must 
+     * always be a spritesheet for the {@link State#IDLE}.
      * @param player The {@link Player}.
      * @param worldDimensions The dimensions of the {@link World}
      * @param sceneDimensions The dimensions of the view in which this {@link Player} will be drawn
@@ -25,14 +26,14 @@ public class PlayerView extends DynamicDrawableEntity {
     public PlayerView(final Map<State, Pair<Image, Integer>> spritesheets, final Player player,
             final Pair<Double, Double> worldDimensions, final Pair<Double, Double> sceneDimensions) {
         super(spritesheets.get(State.IDLE).getLeft(), player, worldDimensions, sceneDimensions);
-        /* the sprites should be squares*/
-        final int dimension = ((Double) spritesheets.get(State.IDLE).getLeft().getHeight()).intValue();
+        final int height = ((Double) spritesheets.get(State.IDLE).getLeft().getHeight()).intValue();
         spritesheets.entrySet().forEach(entry -> {
+            final int width = ((Double) entry.getValue().getLeft().getWidth()).intValue() / entry.getValue().getRight();
             this.mapAnimation(entry.getKey(), new SpriteAnimation(entry.getValue().getLeft(), 
                                                                     Duration.millis(DURATION),
                                                                     entry.getValue().getRight(),
-                                                                    dimension,
-                                                                    dimension));
+                                                                    width,
+                                                                    height));
         });
     }
 }
--- src/view/game/WalkingEnemyView.java
+++ src/view/game/WalkingEnemyView.java
@@ -18,7 +18,8 @@ public class WalkingEnemyView extends DynamicDrawableEntity {
 
     /**
      * @param spritesheets A map that matches the {@link State} of the drawn {@link Entity} to the spritesheets that will 
-     * represent it. The value of an entry is a pair with the spritesheet and the number of frames it contains. 
+     * represent it. The value of an entry is a pair with the spritesheet and the number of frames it contains. There must 
+     * always be a spritesheet for the {@link State#IDLE}.
      * @param walkingEnemy The {@link WalkingEnemyView}.
      * @param worldDimensions The dimensions of the {@link World}
      * @param sceneDimensions The dimensions of the view in which this {@link Player} will be drawn
@@ -26,14 +27,14 @@ public class WalkingEnemyView extends DynamicDrawableEntity {
     public WalkingEnemyView(final Map<State, Pair<Image, Integer>> spritesheets, final WalkingEnemy walkingEnemy,
             final Pair<Double, Double> worldDimensions, final Pair<Double, Double> sceneDimensions) {
         super(spritesheets.get(State.IDLE).getLeft(), walkingEnemy, worldDimensions, sceneDimensions);
-        /* the sprites should be squares*/
-        final int dimension = ((Double) spritesheets.get(State.IDLE).getLeft().getHeight()).intValue();
+        final int height = ((Double) spritesheets.get(State.IDLE).getLeft().getHeight()).intValue();
         spritesheets.entrySet().forEach(entry -> {
+            final int width = ((Double) entry.getValue().getLeft().getWidth()).intValue() / entry.getValue().getRight();
             this.mapAnimation(entry.getKey(), new SpriteAnimation(entry.getValue().getLeft(), 
                                                                     Duration.millis(DURATION),
                                                                     entry.getValue().getRight(),
-                                                                    dimension,
-                                                                    dimension));
+                                                                    width,
+                                                                    height));
         });
     }
 }
