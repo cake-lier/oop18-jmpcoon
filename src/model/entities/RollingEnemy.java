5269246c9701dc2f791d16dd1d344c0aa659b0f1
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-04-14 00:52:07 +0200

    Add applyImpulse() in RollingEnemy.java

--- src/model/entities/RollingEnemy.java
+++ src/model/entities/RollingEnemy.java
@@ -3,21 +3,26 @@ package model.entities;
 import model.physics.DynamicPhysicalBody;
 
 /**
- * TODO: add a non-linear velocity movement
  * a walking enemy inside the {@link model.world.World} of the game.
  */
 public final class RollingEnemy extends DynamicEntity {
 
     private static final long serialVersionUID = -6624661835399417683L;
+    private final DynamicPhysicalBody body;
 
     /**
-     * Creates a new {@link RollingEnemy} with the given {@link DynamicPhysicalBody}. This constructor is package protected
-     * because it should be only invoked by the {@link EntityBuilder} when creating a new instance of it and no one else.
-     * @param body The {@link DynamicPhysicalBody} that should be contained in this {@link RollingEnemy}.
+     * Creates a new {@link RollingEnemy} with the given
+     * {@link DynamicPhysicalBody}. This constructor is package protected because it
+     * should be only invoked by the {@link EntityBuilder} when creating a new
+     * instance of it and no one else.
+     * 
+     * @param body
+     *            The {@link DynamicPhysicalBody} that should be contained in this
+     *            {@link RollingEnemy}.
      */
     RollingEnemy(final DynamicPhysicalBody body) {
         super(body);
-        // TODO Auto-generated constructor stub
+        this.body = body;
     }
 
     /**
@@ -34,6 +39,14 @@ public final class RollingEnemy extends DynamicEntity {
     @Override
     public EntityType getType() {
         return EntityType.ROLLING_ENEMY;
+    }
+
+    //TODO: to be left or not?
+    /**
+     * appliese an impulse to the {@link RollingEnemy}.
+     */
+    public void applyImpulse() {
+        this.body.setFixedVelocity(MovementType.MOVE_RIGHT, 2, 0);
     }
 
 }
