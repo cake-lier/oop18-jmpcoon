eff57ffe2735a7ddda2fb1bbc39d0bd0d9ab123a
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-04-14 00:55:00 +0200

    Implement methods to create enemies in EnemyGenerator.java

--- src/model/entities/EnemyGenerator.java
+++ src/model/entities/EnemyGenerator.java
@@ -1,5 +1,10 @@
 package model.entities;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import model.physics.PhysicalFactory;
 import model.physics.StaticPhysicalBody;
 
 /**
@@ -8,6 +13,11 @@ import model.physics.StaticPhysicalBody;
 public final class EnemyGenerator extends StaticEntity {
 
     private static final long serialVersionUID = -3160192139428572083L;
+    private static final ImmutablePair<Double, Double> ROLLING_ENEMY_DIMENSIONS = new ImmutablePair<Double, Double>(0.15, 0.15);
+    private static final int DELTA = 380;
+
+    private List<RollingEnemy> enemies = new LinkedList<>();
+    private int count = 0;
 
     /**
      * Creates a new {@link EnemyGenerator} with the given {@link StaticPhysicalBody}. This constructor is package protected
@@ -34,4 +44,31 @@ public final class EnemyGenerator extends StaticEntity {
         return EntityShape.RECTANGLE;
     }
 
+    /**
+     * @param physicsFactory the {@link Physics
+     * @return ddh
+     */
+    public Iterable<RollingEnemy> onTimeAdvanced(final PhysicalFactory physicsFactory) {
+        this.enemies.clear();
+        if (checkTime()) {
+            this.enemies.add(createRollingEnemy(physicsFactory));
+            this.enemies.get(0).applyImpulse();
+        }
+        return this.enemies;
+    }
+
+    private boolean checkTime() {
+        return this.count++ % DELTA == 0;
+    }
+
+    //TODO: better way to give physicsFactory to RollingEnemy?
+    private RollingEnemy createRollingEnemy(final PhysicalFactory physicsFactory) {
+        return EntityBuilderUtils.getRollingEnemyBuilder()
+                .setFactory(physicsFactory)
+                .setDimensions(ROLLING_ENEMY_DIMENSIONS)
+                .setAngle(0.0)
+                .setPosition(this.getPosition())
+                .setShape(EntityShape.CIRCLE)
+                .build();
+    }
 }
