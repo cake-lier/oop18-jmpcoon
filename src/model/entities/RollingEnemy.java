8ce3b64000d333b71aaa369f591f90853afcdd8f
Yuqi Sun <yuqi.sun@studio.unibo.it>
2019-04-14 22:05:38 +0200

    Implement movement methods in RollingEnemy.java

--- src/model/entities/RollingEnemy.java
+++ src/model/entities/RollingEnemy.java
@@ -8,17 +8,16 @@ import model.physics.DynamicPhysicalBody;
 public final class RollingEnemy extends DynamicEntity {
 
     private static final long serialVersionUID = -6624661835399417683L;
+    private static final double ROLLING_ENEMY_SPEED = 0.7;
+
     private final DynamicPhysicalBody body;
+    private boolean direction = false;
+    private boolean onAir = true;
 
     /**
-     * Creates a new {@link RollingEnemy} with the given
-     * {@link DynamicPhysicalBody}. This constructor is package protected because it
-     * should be only invoked by the {@link EntityBuilder} when creating a new
-     * instance of it and no one else.
-     * 
-     * @param body
-     *            The {@link DynamicPhysicalBody} that should be contained in this
-     *            {@link RollingEnemy}.
+     * Creates a new {@link RollingEnemy} with the given {@link DynamicPhysicalBody}. This constructor is package protected because it
+     * should be only invoked by the {@link EntityBuilder} when creating a new instance of it and no one else.
+     * @param body The {@link DynamicPhysicalBody} that should be contained in this {@link RollingEnemy}.
      */
     RollingEnemy(final DynamicPhysicalBody body) {
         super(body);
@@ -33,12 +32,32 @@ public final class RollingEnemy extends DynamicEntity {
         return EntityType.ROLLING_ENEMY;
     }
 
-    //TODO: to be left or not?
     /**
-     * appliese an impulse to the {@link RollingEnemy}.
+     * applies a fixed velocity to the {@link RollingEnemy}.
      */
-    public void applyImpulse() {
-        this.body.setFixedVelocity(MovementType.MOVE_RIGHT, 2, 0);
+    public void computeMovement() {
+        if (!this.onAir) {
+            this.body.setFixedVelocity(this.getMovement(), this.getDirection() * ROLLING_ENEMY_SPEED, 0);
+        }
     }
 
+    /**
+     * @param isOnAir set {@link RollingEnemy} state: true if it's on air, false otherwise
+     */
+    public void setOnAir(final boolean isOnAir) {
+        if (this.onAir != isOnAir) {
+            this.onAir = isOnAir;
+            if (!this.onAir) {
+                this.direction = !this.direction;
+            }
+        }
+    }
+
+    private MovementType getMovement() {
+        return this.direction ? MovementType.MOVE_RIGHT : MovementType.MOVE_LEFT;
+    }
+
+    private int getDirection() {
+        return this.direction ? 1 : -1;
+    }
 }
